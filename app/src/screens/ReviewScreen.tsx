3d34c3c02bfda051aba1ae75dcc99092668f3468
Kaan Aksoy <kaanaksoyaz@gmail.com>
2020-07-09 14:53:57 -0700

    Create base form for reviews

--- app/src/screens/ReviewScreen.tsx
+++ app/src/screens/ReviewScreen.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { Layout, Input, Text } from '@ui-kitten/components';
+import { Layout, Input, Button, Select, SelectItem } from '@ui-kitten/components';
 
 export default class ReviewScreen extends React.Component<{}, {}> {
   static navigationOptions = {
@@ -9,9 +9,38 @@ export default class ReviewScreen extends React.Component<{}, {}> {
 
   render() {
     return (
-      <Layout></Layout>
+      <Layout style={styles.layout}>
+        <Select
+          style={styles.formControl}
+          placeholder='Do employees wear masks?'
+        >
+          <SelectItem title='Yes' />
+          <SelectItem title='No' />
+          <SelectItem title='Not sure' />
+        </Select>
+        <Input
+          style={styles.formControl}
+          textStyle={styles.reviewContent}
+          placeholder='Include any other details here!'
+          multiline
+        />
+        <Button>
+          Submit Review
+        </Button>
+      </Layout>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+    padding: 10
+  },
+  formControl: {
+    marginVertical: 10
+  },
+  reviewContent: {
+    minHeight: 100
+  }
+});
