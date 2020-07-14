8c0cfd3a1e25fd1b5fa2b46a50e6628f3d475ae4
Kaan Aksoy <kaanaksoyaz@gmail.com>
2020-07-13 17:02:42 -0700

    Add fade-in animation to ConfirmationModal

--- app/src/components/review/ConfirmationModal.tsx
+++ app/src/components/review/ConfirmationModal.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, View, Animated } from 'react-native';
 import { Button, Modal, Card, Text } from '@ui-kitten/components';
 
 type Props = {
@@ -7,27 +7,47 @@ type Props = {
   onConfirm: () => void
 };
 
-export default function ConfirmationModal({ isVisible, onConfirm }: Props) {
-  return (
-    <Modal
-      backdropStyle={styles.modalBackdrop}
-      visible={isVisible}
-    >
-      <Card style={styles.card} disabled>
-        <View style={styles.message}>
-          <Text category='s1'>Your review has been submitted.</Text>
-          <Text category='s1'>Thank you!</Text>
-        </View>
-        <Button
-          style={styles.confirmButton}
-          status='basic'
-          onPress={onConfirm}
-        >
-          Continue
+const FADE_DURATION = 250;
+
+export default class ConfirmationModal extends React.Component<Props, {}> {
+  opacity = new Animated.Value(0.0);
+
+  componentDidUpdate(prevProps: Props) {
+    if (!prevProps.isVisible && this.props.isVisible) {
+      Animated.timing(this.opacity, {
+        toValue: 1,
+        duration: FADE_DURATION,
+        useNativeDriver: true
+      }).start();
+    }
+  }
+
+  render() {
+    const { isVisible, onConfirm } = this.props;
+
+    return (
+      <Modal
+        backdropStyle={styles.modalBackdrop}
+        visible={isVisible}
+      >
+        <Animated.View style={{ opacity: this.opacity }}>
+          <Card style={styles.card} disabled>
+            <View style={styles.message}>
+              <Text category='s1'>Your review has been submitted.</Text>
+              <Text category='s1'>Thank you!</Text>
+            </View>
+            <Button
+              style={styles.confirmButton}
+              status='basic'
+              onPress={onConfirm}
+            >
+              Continue
         </Button>
-      </Card>
-    </Modal>
-  );
+          </Card>
+        </Animated.View>
+      </Modal>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
