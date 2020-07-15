20a19d19a21d442b1870f4bc880e4a49f40c4021
Kaan Aksoy <kaanaksoyaz@gmail.com>
2020-07-15 11:38:41 -0700

    Add logic for displaying splashscreen

--- app/App.tsx
+++ app/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { SplashScreen } from 'expo';
 import { ApplicationProvider } from '@ui-kitten/components';
 import { NavigationContainer, Route } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -6,6 +7,8 @@ import * as eva from '@eva-design/eva';
 import MapScreen from './src/screens/MapScreen';
 import ReviewScreen from './src/screens/ReviewScreen';
 import { Place } from './src/services/PlaceService';
+
+const SPLASH_DURATION_MS = 2000;
 
 export type StackParamList = {
   Map: undefined,
@@ -18,9 +21,21 @@ const ScreenOptions = ({ route }: { route: Route<string> }) => ({
   headerShown: route.name === 'Review',
 });
 
-// Comment
-
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    SplashScreen.preventAutoHide();
+    setTimeout(() => {
+      SplashScreen.hide();
+      setIsLoading(false);
+    }, SPLASH_DURATION_MS);
+  });
+
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <ApplicationProvider {...eva} theme={eva.light}>
       <NavigationContainer>
