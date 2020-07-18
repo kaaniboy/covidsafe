83051e98460fc98e9e6bfa8ffd3ca657d28f91a7
Kaan Aksoy <kaanaksoyaz@gmail.com>
2020-07-18 15:50:20 -0700

    Create website template

---
+++ web/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function App() {
+  return (
+    <div>Here we go again...</div>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
