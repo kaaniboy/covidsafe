16c7fccc02c43b31f356af09640f1df77f0e6fa2
Kaan Aksoy <kaanaksoyaz@gmail.com>
2020-07-20 15:43:00 -0700

    Updates

---
+++ precache-manifest.c41926415a70a3f96e5aa9e5cdd5d61a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c22fea0a8c1585fcc8566e01c9865e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98d456e54cec07e2d25e",
+    "url": "/static/css/2.c93a3001.chunk.css"
+  },
+  {
+    "revision": "fcefe80983d01ae5bc68",
+    "url": "/static/css/main.40d1621e.chunk.css"
+  },
+  {
+    "revision": "98d456e54cec07e2d25e",
+    "url": "/static/js/2.d892d270.chunk.js"
+  },
+  {
+    "revision": "6442767788cae0b3e8591189e98c3a84",
+    "url": "/static/js/2.d892d270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcefe80983d01ae5bc68",
+    "url": "/static/js/main.09498985.chunk.js"
+  },
+  {
+    "revision": "d0a598c2c4f910599d51",
+    "url": "/static/js/runtime-main.e7310a31.js"
+  },
+  {
+    "revision": "ebd3b92b922ffa030e0bc8c7286d37ab",
+    "url": "/static/media/Jost-VariableFont_wght.ebd3b92b.ttf"
+  }
+]);
