84464b4e1b2b9bfb444d086bea4303409b0e5cec
Kaan Aksoy <kaanaksoyaz@gmail.com>
2020-07-22 15:49:41 -0700

    Add support for specific category queries, like 'Coffee Shops'

--- server/src/foursquare.js
+++ server/src/foursquare.js
@@ -9,12 +9,25 @@ const FS_PLACE_RADIUS = process.env.FOURSQUARE_PLACE_RADIUS;
 const FS_CATEGORIES_FILE = process.env.FOURSQUARE_CATEGORIES_FILE;
 
 // Foursquare Endpoints: https://developer.foursquare.com/docs/places-api/endpoints/
-const FS_PLACES_ENDPOINT = 
+const FS_PLACES_ENDPOINT =
   'https://api.foursquare.com/v2/venues/search?'
   + `client_id=${FS_CLIENT_ID}&client_secret=${FS_CLIENT_SECRET}`
   + `&categoryId=${FS_PLACE_CATEGORIES}&v=20200701`;
 
+const FS_RECOMMENDATIONS_ENDPOINT =
+  'https://api.foursquare.com/v2/search/recommendations?'
+  + `client_id=${FS_CLIENT_ID}&client_secret=${FS_CLIENT_SECRET}&v=20200701`;
+
 let CATEGORIES = [];
+
+// Listing of venue recommendations that can be triggered by the specified keywords.
+const RECOMMENDED_CATEGORIES = {
+  'coffee': ['coffee', 'coffee shop', 'coffee shops'],
+  'food': ['food', 'restaurant', 'restaurants'],
+  'breakfast': ['breakfast'],
+  'lunch': ['lunch'],
+  'dinner': ['dinner', 'dining']
+};
 
 function loadCategories() {
   try {
@@ -40,17 +53,45 @@ function findCategoryHelper(placeCategoryId, category) {
   );
 }
 
-async function handlePlacesRequest(req, res) {
-  const { ll, query } = req.query;
+function getRecommendedCategory(query) {
+  if (!query) {
+    return null;
+  }
 
+  query = query.toLowerCase().trim();
+  for (const [category, keywords] of Object.entries(RECOMMENDED_CATEGORIES)) {
+    if (keywords.includes(query)) {
+      return category;
+    }
+  }
+  return null;
+}
+
+async function retrievePlaces(ll, query) {
   // Exclude radius if a query is provided
   const params = query
     ? `&ll=${ll}&query=${query}`
     : `&ll=${ll}&radius=${FS_PLACE_RADIUS}`;
 
+  const json = await axios.get(FS_PLACES_ENDPOINT + params);
+  return json.data.response.venues;
+}
+
+async function retrieveRecommendations(ll, category) {
+  const params = `&ll=${ll}&radius=${FS_PLACE_RADIUS}&intent=${category}`;
+
+  const json = await axios.get(FS_RECOMMENDATIONS_ENDPOINT + params);
+  return json.data.response.group.results.map(result => result.venue);
+}
+
+async function handlePlacesRequest(req, res) {
+  const { ll, query } = req.query;
+  const category = getRecommendedCategory(query);
+
   try {
-    const json = await axios.get(FS_PLACES_ENDPOINT + params);
-    const places = json.data.response.venues;
+    const places = category
+      ? await retrieveRecommendations(ll, category)
+      : await retrievePlaces(ll, query);
 
     const categorizedPlaces = places.map(p => {
       p.category = findCategory(p);
